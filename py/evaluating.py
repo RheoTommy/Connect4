7787ed66287ecb365effb37454c1665686526e49
rheotommy <tommyrheo@gmail.com>
2021-08-28 21:23:08 +0900

    アルゴリズム評価処理の作成

--- py/evaluating.py
+++ py/evaluating.py
@@ -3,8 +3,11 @@ from shutil import copy
 from tensorflow.keras.models import Model, load_model
 from pv_mct_search import pv_mct_search
 from py.config import EVAL_COUNT, DENSE_LATEST_FILE, DENSE_BEST_FILE
-from py.game import play
+from py.game import play, random_action
 import tensorflow.keras.backend as bk
+
+from py.pure_mct_search import pure_mct_search_action
+from py.uct_mct_search import uct_mct_search_action
 
 
 def update_player(best_file, latest_file):
@@ -25,7 +28,7 @@ def evaluate_change(best_file, latest_file):
         if i % 2 == 0:
             total_point += play(next_actions)
         else:
-            total_point -= play(next_actions)
+            total_point -= play(list(reversed(next_actions)))
         print("\rEvaluate {}/{}".format(i + 1, EVAL_COUNT), end=" ")
     print()
 
@@ -43,5 +46,38 @@ def evaluate_change(best_file, latest_file):
         return False
 
 
+# 2 つのアルゴリズム同士での勝率計算
+def evaluate_algo(label, next_actions):
+    total_point = 0
+    for i in range(EVAL_COUNT):
+        if i % 2 == 0:
+            total_point += play(next_actions)
+        else:
+            total_point -= play(list(reversed(next_actions)))
+        print("\rEvaluate {}/{}".format(i + 1, EVAL_COUNT), end=" ")
+    print()
+
+    ave_point = total_point / EVAL_COUNT
+    print(label, ave_point)
+
+
+# アルゴリズムの評価
+def evaluate_player(best_file):
+    model: Model = load_model(best_file)
+    pv_action = pv_mct_search(model, 0.0)[1]
+
+    next_actions = [pv_action, random_action]
+    evaluate_algo("{} VS Random".format(best_file), next_actions)
+
+    next_actions = [pv_action, pure_mct_search_action]
+    evaluate_algo("{} VS Pure mct search".format(best_file), next_actions)
+
+    next_actions = [pv_action, uct_mct_search_action]
+    evaluate_algo("{} VS UCT mct search action".format(best_file), next_actions)
+
+    bk.clear_session()
+    del model
+
+
 if __name__ == '__main__':
-    evaluate_change(DENSE_BEST_FILE, DENSE_LATEST_FILE)
+    evaluate_player(DENSE_BEST_FILE)
